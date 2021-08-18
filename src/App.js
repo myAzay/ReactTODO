412062f83fc534d1d013506d41a8085ae5bf280a
Azay <zaichenkoandrii322@gmail.com>
2021-08-18 22:45:45 +0300

    bug with auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { useContext } from 'react';
 import {BrowserRouter} from 'react-router-dom'
-import { Context } from '.';
 import AppRouter from './components/AppRouter'
 
+
 const App = () => {
-  const {user}=useContext(Context);
-  
-  /*if(localStorage.getItem('token'))
-  {
-    user.setIsAuth(true);
-  }*/
-  
+ 
   return (
     <BrowserRouter>
       <AppRouter/>
